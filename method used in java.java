14bfb79df3fa273df13b2c8e4c4a7dbb884539ca
Pranav7171 <156706003+Pranav7171@users.noreply.github.com>
2025-01-15 01:25:51 +0530

    Update and rename Pw2.java to method used in java.java

---
+++ method used in java.java
@@ -0,0 +1,7 @@
+  void Jivlag(){
+        System.out.println("Pranav Patil");
+        System.out.println("Tejas Patil");
+        System.out.println("Rajtilak joshi");
+        System.out.println("Parth bhor");
+        System.out.println("Omkar Patil");
+   }
